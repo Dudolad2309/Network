04ac826390b0e5a35dafb2e18eb20b90b74732a4
Dudolad2309 <77323788+Dudolad2309@users.noreply.github.com>
2021-08-27 09:06:30 +0300

    Refactor

--- src/components/Dialogs/DialogItem/DialogItem.jsx
+++ src/components/Dialogs/DialogItem/DialogItem.jsx
@@ -4,14 +4,14 @@ import s from "./DialogItem.module.css";
 import {Typography} from "@material-ui/core";
 
 const DialogsItem = (props) => {
-  return (
-    <div className={s.dialog}>
-      <NavLink activeClassName={s.activeLink} to={"/dialogs/" + props.id}>
-        <img src="https://s.pfst.net/2017.06/57345286756788a2d6aa37c7a739fe2b7beab61d3c94_b.jpg" alt={"image"}/>
-        <Typography paragraph>{props.name}</Typography>
-      </NavLink>
-    </div>
-  );
+	return (
+		<div className={s.dialog}>
+			<NavLink activeClassName={s.activeLink} to={"/dialogs/" + props.id}>
+				<img src="https://s.pfst.net/2017.06/57345286756788a2d6aa37c7a739fe2b7beab61d3c94_b.jpg" alt={"image"}/>
+				<Typography paragraph>{props.name}</Typography>
+			</NavLink>
+		</div>
+	);
 };
 
 export default DialogsItem;
