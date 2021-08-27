43b627e5d8d05455e6c7df28130996c4d2347b51
Dudolad2309 <77323788+Dudolad2309@users.noreply.github.com>
2021-08-27 10:24:14 +0300

    Refactor

--- src/Redux/ProfileReducer.test.js
+++ src/Redux/ProfileReducer.test.js
@@ -1,45 +1,22 @@
 import ProfileReducer, {addPostActionCreator, deletePost} from "./ProfileReducer";
 
-let state = {
-    PostData: [ {id: 1, message: "Hello how are you?", countLike: 5}, ]
-
+const state = {
+	PostData: [{id: 1, message: "Hello how are you?", countLike: 5},]
 };
-
-
-
 it('length post should be incremented', () => {
-// 1. test data
-    let action = addPostActionCreator("it-dims")
-
-
-// 2. action
-    let newState = ProfileReducer(state,action)
-// 3. expected
-    expect(newState.PostData.length).toBe(2)
-
+	let action = addPostActionCreator("it-dims")
+	let newState = ProfileReducer(state, action)
+	expect(newState.PostData.length).toBe(2)
 });
 
 it('message new post should be correct', () => {
-// 1. test data
-    let action = addPostActionCreator("it-dims")
-
-
-// 2. action
-    let newState = ProfileReducer(state,action)
-// 3. expected
-
-    expect(newState.PostData[1].message).toBe("it-dims")
+	let action = addPostActionCreator("it-dims")
+	let newState = ProfileReducer(state, action)
+	expect(newState.PostData[1].message).toBe("it-dims")
 });
-
-
-it ("deleting post messages should be decrement", () => {
-// 1. test data
-    let action = deletePost(1)
-
-// 2. action
-    let newState = ProfileReducer(state,action)
-
-// 3. expected
-    expect(newState.PostData.length).toBe(0)
+it("deleting post messages should be decrement", () => {
+	let action = deletePost(1)
+	let newState = ProfileReducer(state, action)
+	expect(newState.PostData.length).toBe(0)
 });
 
