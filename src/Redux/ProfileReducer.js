05763c68babf2ac15f969c0593e440d2bcc3f108
Dudolad2309 <77323788+Dudolad2309@users.noreply.github.com>
2021-08-27 10:22:36 +0300

    Refactor

--- src/Redux/ProfileReducer.js
+++ src/Redux/ProfileReducer.js
@@ -1,93 +1,74 @@
 import {ProfileAPI, usersAPI} from "../API/API";
 
-
 const ADD_POST = "ADD-POST";
 const SET_USER_PROFILE = "SET_USER_PROFILE"
 const SET_STATUS = "SET_STATUS"
 const DELETE_POST = "DELETE_POST"
 
-let initialStat = {
-    PostData: [
-        {id: 1, message: "Hello how are you?", countLike: 5},
-    ],
-    profile: null,
-    status: ""
-}
+const initialStat = {
+	PostData: [
+		{id: 1, message: "Hello how are you?", countLike: 5},
+	],
+	profile: null,
+	status: "",
+};
 
 const ProfileReducer = (state = initialStat, action) => {
-    switch (action.type) {
-        case ADD_POST: {
-
-            return {
-                ...state,
-                PostData: [...state.PostData, {id: 1, message: action.onChangePost, countLike: 5}]
-            }
-
-        }
-        case SET_USER_PROFILE: {
-            return {
-                ...state,
-                profile: action.profile
-            }
-        }
-        case SET_STATUS: {
-            return {
-                ...state,
-                status: action.status
-            }
-        }
-        case DELETE_POST: {
-            return {
-                ...state,
-                PostData: state.PostData.filter(p => p.id !== action.postId)
-            }
-        }
-        default:
-            return state;
-    }
+	switch (action.type) {
+		case ADD_POST: {
+			return {
+				...state,
+				PostData: [...state.PostData, {id: 1, message: action.onChangePost, countLike: 5}]
+			};
+		}
+		case SET_USER_PROFILE: {
+			return {
+				...state,
+				profile: action.profile
+			};
+		}
+		case SET_STATUS: {
+			return {
+				...state,
+				status: action.status
+			};
+		}
+		case DELETE_POST: {
+			return {
+				...state,
+				PostData: state.PostData.filter(p => p.id !== action.postId)
+			};
+		}
+		default:
+			return state;
+	}
 };
 
 export const addPostActionCreator = (onChangePost) => {
-    return {type: ADD_POST, onChangePost};
+	return {type: ADD_POST, onChangePost};
 };
-
 export const setUserProfile = (profile) => {
-
-    return {type: SET_USER_PROFILE, profile};
+	return {type: SET_USER_PROFILE, profile};
 };
 export const setStatus = (status) => {
-
-    return {type: SET_STATUS, status};
+	return {type: SET_STATUS, status};
 };
 export const deletePost = (postId) => {
-
-    return {type: DELETE_POST, postId};
+	return {type: DELETE_POST, postId};
 };
-
 export const getProfile = (userId) => async (dispatch) => {
-
-    let response = await usersAPI.getProfile(userId)
-    dispatch(setUserProfile(response.data))
-
-
-}
+	let response = await usersAPI.getProfile(userId)
+	dispatch(setUserProfile(response.data))
+};
 export const getStatus = (userId) => async (dispatch) => {
-
-    let response = await ProfileAPI.getStatus(userId)
-    dispatch(setStatus(response.data))
-
-
-}
+	let response = await ProfileAPI.getStatus(userId)
+	dispatch(setStatus(response.data))
+};
 export const updateStatus = (status) => async (dispatch) => {
-
-
-    let response = await ProfileAPI.updateStatus(status)
-    if (response.data.resultCode === 0) {
-
-        dispatch(setStatus(status))
-    }
-
-
-}
+	let response = await ProfileAPI.updateStatus(status)
+	if (response.data.resultCode === 0) {
+		dispatch(setStatus(status));
+	}
+};
 
 export default ProfileReducer;
