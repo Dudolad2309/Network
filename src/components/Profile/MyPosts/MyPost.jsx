6b0455c21e2beebbb6e695178d01ed866cbad5c1
Dudolad2309 <77323788+Dudolad2309@users.noreply.github.com>
2021-08-27 09:54:20 +0300

    Refactor

--- src/components/Profile/MyPosts/MyPost.jsx
+++ src/components/Profile/MyPosts/MyPost.jsx
@@ -3,38 +3,35 @@ import Post from "./Posts/Post";
 import {Field, reduxForm} from "redux-form";
 import {Box, Container, Typography} from "@material-ui/core";
 
-
 const MyPost = React.memo(props => {
 
-    let PostElement = props.state.Profile.PostData.map((post) => (
-        <Post key={post.id} message={post.message} countLike={post.countLike}/>
-    ));
+	const PostElement = props.state.Profile.PostData.map((post) => (
+		<Post key={post.id} message={post.message} countLike={post.countLike}/>
+	));
+	const onChangePost = (values) => {
+		props.addPost(values.onChangePost);
+	};
 
-
-    let onChangePost = (values) => {
-        props.addPost(values.onChangePost)
-
-    };
-
-    return (
-        <Container>
-            <Typography variant="h4">My post</Typography>
-            <AddMessageReduxForm onSubmit={onChangePost}/>
-            <Typography paragraph>{PostElement}</Typography>
-        </Container>
-    );
+	return (
+		<Container>
+			<Typography variant="h4">post</Typography>
+			<AddMessageReduxForm onSubmit={onChangePost}/>
+			<Typography paragraph>{PostElement}</Typography>
+		</Container>
+	);
 });
 
-
 const AddPostForm = (props) => {
-    return (
-        <form onSubmit={props.handleSubmit}>
-            <Field placeholder={""} component={"input"} name={"onChangePost"}/>
-           <Box marginTop={3}><button>Add post</button></Box>
-        </form>
+	return (
+		<form onSubmit={props.handleSubmit}>
+			<Field placeholder={""} component={"input"} name={"onChangePost"}/>
+			<Box marginTop={3}>
+				<button>Add post</button>
+			</Box>
+		</form>
+	)
+};
 
-    )
-}
 const AddMessageReduxForm = reduxForm({form: "addPostForm"})(AddPostForm)
 
 export default MyPost;
