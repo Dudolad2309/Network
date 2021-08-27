8ca9b0a15481e674aee08dd73ab9577d46781566
Dudolad2309 <77323788+Dudolad2309@users.noreply.github.com>
2021-08-27 09:42:21 +0300

    Refactor

--- src/components/Profile/ProfileInfo/StatusProfile.test.js
+++ src/components/Profile/ProfileInfo/StatusProfile.test.js
@@ -1,20 +1,17 @@
 import React from "react";
-import { create } from "react-test-renderer";
+import {create} from "react-test-renderer";
 import StatusProfile from "./StatusProfile";
 
-
-
 describe("StatusProfile component", () => {
-    test("Status from props", () => {
-        const component = create(<StatusProfile status="Hello" />);
-        const instance = component.getInstance();
-        expect(instance.state.status).toBe("Hello");
-    });
-
-    test("span displayed", () => {
-        const component = create(<StatusProfile status="Hello" />);
-        const root = component.root;
-        let span = root.findByType("span")
-        expect(span.innerText).not.toBeNull()
-    });
+	test("Status from props", () => {
+		const component = create(<StatusProfile status="Hello"/>);
+		const instance = component.getInstance();
+		expect(instance.state.status).toBe("Hello");
+	});
+	test("span displayed", () => {
+		const component = create(<StatusProfile status="Hello"/>);
+		const root = component.root;
+		let span = root.findByType("span")
+		expect(span.innerText).not.toBeNull()
+	});
 });
