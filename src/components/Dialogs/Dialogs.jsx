378ceb314478d9107302d1c3e7ba81f0b76a8bc1
Dudolad2309 <77323788+Dudolad2309@users.noreply.github.com>
2021-08-27 09:01:40 +0300

    Refactor

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -6,46 +6,39 @@ import {Field, reduxForm} from "redux-form";
 import {Box, Typography} from "@material-ui/core";
 
 
-
-
 const Dialogs = (props) => {
 
+  let DialogsElement = props.state.Dialog.DialogsData.map((dialog) => (
+    <DialogsItem key={dialog.id} name={dialog.name} id={dialog.id}/>
+  ));
 
-    let DialogsElement = props.state.Dialog.DialogsData.map((dialog) => (
-        <DialogsItem key ={dialog.id} name={dialog.name} id={dialog.id} />
-    ));
+  let MessagesElement = props.state.Dialog.MessageData.map((message) => (
+    <Message key={message.id} text={message.text}/>
+  ));
 
-    let MessagesElement = props.state.Dialog.MessageData.map((message) => (
-        <Message key ={message.id}  text={message.text} />
-    ));
-
-
-    let addNewMessage = (values) => {
-
-        props.sendMessages(values.onChangeMessage);
-    };
-
+  let addNewMessage = (values) => {
+    props.sendMessages(values.onChangeMessage);
+  };
 
   return (
     <Box>
       <Typography className={s.dialogs}>{DialogsElement} </Typography>
       <Typography className={s.messages}>{MessagesElement}</Typography>
-      <AddMessageReduxForm onSubmit={addNewMessage} />
+      <AddMessageReduxForm onSubmit={addNewMessage}/>
     </Box>
   );
 };
 
-const AddMessageForm=(props)=>{
-    return(
-        <form onSubmit={props.handleSubmit}>
-            <Field placeholder={""} component={"input"} name={"onChangeMessage"}/>
-            <button >Send message</button>
-        </form>
-
-    )
+const AddMessageForm = (props) => {
+  return (
+    <form onSubmit={props.handleSubmit}>
+      <Field placeholder={""} component={"input"} name={"onChangeMessage"}/>
+      <button>Send message</button>
+    </form>
+  )
 }
 
-const AddMessageReduxForm=reduxForm({form: "dialogAddMessageForm"})(AddMessageForm)
+const AddMessageReduxForm = reduxForm({form: "dialogAddMessageForm"})(AddMessageForm)
 
 export default Dialogs;
 
