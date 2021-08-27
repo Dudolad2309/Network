483e3910c00f10dbbda98a0fb7982ad1f55df613
Dudolad2309 <77323788+Dudolad2309@users.noreply.github.com>
2021-08-27 10:25:04 +0300

    Refactor

--- src/Redux/Redux-store.js
+++ src/Redux/Redux-store.js
@@ -5,26 +5,21 @@ import SideBarReducer from "./SideBarReducer";
 import usersReducer from "./usersReducer";
 import authReducer from "./authReducer";
 import thunkMiddleware from "redux-thunk"
-import { reducer as formReducer } from 'redux-form'
+import {reducer as formReducer} from 'redux-form'
 import AppReducer from "./AppReducer";
 
 
-let reducers=combineReducers({
-    Profile:ProfileReducer,
-    Dialog:DialogReducer,
-    SideBar:SideBarReducer,
-    UsersPage: usersReducer,
-    auth: authReducer,
-    form: formReducer,
-    app: AppReducer
-})
+let reducers = combineReducers({
+	Profile: ProfileReducer,
+	Dialog: DialogReducer,
+	SideBar: SideBarReducer,
+	UsersPage: usersReducer,
+	auth: authReducer,
+	form: formReducer,
+	app: AppReducer,
+});
 
-   const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-   const store = createStore(reducers,composeEnhancers(applyMiddleware(thunkMiddleware)));
-
-
-
-
-
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunkMiddleware)));
 
 export default store
