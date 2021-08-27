03fb1d83176f9a0d4e79a794305014a31c329256
Dudolad2309 <77323788+Dudolad2309@users.noreply.github.com>
2021-08-27 09:58:29 +0300

    Refactor

--- src/components/Settings/Settings.jsx
+++ src/components/Settings/Settings.jsx
@@ -2,18 +2,20 @@ import React from "react";
 import s from "./Settings.module.css";
 
 const Settings = () => {
-  return (
-    <div className={s.item}>
-      Settings
-      <div>
-        <img
-          className={s.img}
-          src="https://images.unsplash.com/photo-1510414842594-a61c69b5ae57?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
-          alt="1"
-        />
-      </div>
-    </div>
-  );
+	return (
+		<div className={s.item}>
+			Settings
+			<div>
+				<img
+					className={s.img}
+					src="https://images.unsplash.com/photo-1510414842594-
+					a61c69b5ae57?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2
+					.1&auto=format&fit=crop&w=750&q=80"
+					alt="img"
+				/>
+			</div>
+		</div>
+	);
 };
 
 export default Settings;
