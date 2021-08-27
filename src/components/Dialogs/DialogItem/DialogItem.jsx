44eff973e93435407c0529ed4e1c0e0f68b5e121
Dudolad2309 <77323788+Dudolad2309@users.noreply.github.com>
2021-08-27 09:04:19 +0300

    Refactor

--- src/components/Dialogs/DialogItem/DialogItem.jsx
+++ src/components/Dialogs/DialogItem/DialogItem.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import {NavLink} from "react-router-dom";
 import s from "./DialogItem.module.css";
 import {Typography} from "@material-ui/core";
 
@@ -7,7 +7,7 @@ const DialogsItem = (props) => {
   return (
     <div className={s.dialog}>
       <NavLink activeClassName={s.activeLink} to={"/dialogs/" + props.id}>
-          <img src="https://s.pfst.net/2017.06/57345286756788a2d6aa37c7a739fe2b7beab61d3c94_b.jpg" alt={"#"}/>
+        <img src="https://s.pfst.net/2017.06/57345286756788a2d6aa37c7a739fe2b7beab61d3c94_b.jpg" alt={"image"}/>
         <Typography paragraph>{props.name}</Typography>
       </NavLink>
     </div>
