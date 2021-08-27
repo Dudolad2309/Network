e7e97f4e76cda0d3509088643716dc0cee51aa84
Dudolad2309 <77323788+Dudolad2309@users.noreply.github.com>
2021-08-27 10:35:35 +0300

    Refactor

--- src/index.js
+++ src/index.js
@@ -2,24 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 import store from "./Redux/Redux-store";
 import reportWebVitals from "./reportWebVitals";
 import {Provider} from "react-redux";
 
 
-
-  ReactDOM.render(
-
-    <BrowserRouter>
-        <Provider store={store}>
-        <App />
-        </Provider>
-    </BrowserRouter>,
-    document.getElementById("root")
-  );
-
-
-
+ReactDOM.render(
+	<BrowserRouter>
+		<Provider store={store}>
+			<App/>
+		</Provider>
+	</BrowserRouter>,
+	document.getElementById("root")
+);
 
 reportWebVitals();
