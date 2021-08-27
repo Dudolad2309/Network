bfdb36fd75fc7990c4d76a8cf10135f88a60f2d4
Dudolad2309 <77323788+Dudolad2309@users.noreply.github.com>
2021-08-27 09:38:47 +0300

    Refactor

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,11 +4,11 @@ import ProfileInfo from "./ProfileInfo/ProfileInfo";
 import MyPostContainer from "./MyPosts/Posts/MyPostContainer";
 
 const Profile = (props) => {
-  return (
-    <div className={s.use}>
-      <ProfileInfo profile={props.profile} status={props.status}   updateStatus={props.updateStatus}/>
-      <MyPostContainer  />
-    </div>
-  );
+	return (
+		<div className={s.use}>
+			<ProfileInfo profile={props.profile} status={props.status} updateStatus={props.updateStatus}/>
+			<MyPostContainer/>
+		</div>
+	);
 };
 export default Profile;
