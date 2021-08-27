05379613e7969a4f8e4a6f79a3cd0d2ec312f25b
Dudolad2309 <77323788+Dudolad2309@users.noreply.github.com>
2021-08-27 09:17:41 +0300

    Refactor

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,41 +4,43 @@ import {AppBar, Avatar, Box, Button, Container, IconButton, makeStyles, Toolbar,
 import MenuIcon from "@material-ui/icons/Menu";
 
 const useStyles = makeStyles((theme) => ({
-    menuButton: {
-        marginRight: theme.spacing(1),
-    },
-    title: {
-        flexGrow: 1,
-        position: "relative",
-    },
-    header: {
-        padding: theme.spacing(1),
-    }
+	menuButton: {
+		marginRight: theme.spacing(1),
+	},
+	title: {
+		flexGrow: 1,
+		position: "relative",
+	},
+	header: {
+		padding: theme.spacing(1),
+	}
 }));
 
 const Header = (props) => {
-    const classes = useStyles();
-    return (
-      <AppBar className={classes.header} position="fixed">
-          <Container>
-              <Toolbar>
-                  <IconButton className={classes.menuButton} edge="start" color="inherit" aria-label="menu">
-                      <MenuIcon/>
-                  </IconButton>
-                  <Typography className={classes.title} variant="h5">Social Network</Typography>
-                  <Box marginRight={1}>
-                      {props.isAuth === true ? <Avatar alt="Remy Sharp" src="https://source.unsplash.com/random"/> : null}
-                  </Box>
-                  <Box>
-                      {props.isAuth === true
-                        ? <Typography> {props.login} <Button onClick={props.logout} color="secondary"
-                                                             variant="contained">Log out</Button> </Typography>
-                        : <NavLink to={"/login"}><Button color="primary" variant="contained">Log
-                            in</Button></NavLink>}
-                  </Box>
-              </Toolbar>
-          </Container>
-      </AppBar>
-    );
+
+	const classes = useStyles();
+
+	return (
+		<AppBar className={classes.header} position="fixed">
+			<Container>
+				<Toolbar>
+					<IconButton className={classes.menuButton} edge="start" color="inherit" aria-label="menu">
+						<MenuIcon/>
+					</IconButton>
+					<Typography className={classes.title} variant="h5">Social Network</Typography>
+					<Box marginRight={1}>
+						{props.isAuth === true ? <Avatar alt="Remy Sharp" src="https://source.unsplash.com/random"/> : null}
+					</Box>
+					<Box>
+						{props.isAuth === true
+							? <Typography> {props.login} <Button onClick={props.logout} color="secondary"
+							                                     variant="contained">Log out</Button> </Typography>
+							: <NavLink to={"/login"}><Button color="primary" variant="contained">Log
+								in</Button></NavLink>}
+					</Box>
+				</Toolbar>
+			</Container>
+		</AppBar>
+	);
 };
 export default Header;
--- src/components/Header/HeaderContainer.jsx
+++ src/components/Header/HeaderContainer.jsx
@@ -2,32 +2,18 @@ import React from "react";
 import {withRouter} from "react-router-dom";
 import Header from "./Header";
 import {connect} from "react-redux";
-import { logout} from "../../Redux/authReducer";
+import {logout} from "../../Redux/authReducer";
 import {compose} from "redux";
 
+const HeaderContainer = props => (
+  <Header {...props} login={props.login} isAuth={props.isAuth}/>
+);
 
-class HeaderContainer extends React.Component {
-
-
-
-
-    render() {
-        return (
-
-
-            <Header {...this.props} login={this.props.login} isAuth={this.props.isAuth}/>
-
-        );
-    }
-}
-
-let mapStateToProps = (state) => ({
+const mapStateToProps = (state) => ({
     id: state.auth.id,
     login: state.auth.login,
-    isAuth: state.auth.isAuth
+    isAuth: state.auth.isAuth,
 
-})
+});
 
-
-
-export default compose( withRouter, connect(mapStateToProps, {logout}),)(HeaderContainer)
+export default compose(withRouter, connect(mapStateToProps, {logout}),)(HeaderContainer)
