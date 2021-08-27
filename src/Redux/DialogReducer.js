30f0d0155ea80c0d5064d005f30fda39cefad643
Dudolad2309 <77323788+Dudolad2309@users.noreply.github.com>
2021-08-27 10:20:00 +0300

    Refactor

--- src/Redux/DialogReducer.js
+++ src/Redux/DialogReducer.js
@@ -1,40 +1,35 @@
 const ADD_MESSAGE = "ADD-MESSAGE";
 
+const initialStat = {
+	MessageData: [
+		{id: 1, text: "Hello"},
+		{id: 2, text: "How are you"},
+		{id: 3, text: "Nice to meet you"},
+	],
+	DialogsData: [
+		{id: 1, name: "Dima"},
+		{id: 2, name: "Vova"},
+		{id: 3, name: "Sveta"},
+	],
+};
 
+const DialogReducer = (state = initialStat, action) => {
 
-let initialStat = {
-  MessageData: [
-    { id: 1, text: "Hello" },
-    { id: 2, text: "How are you" },
-    { id: 3, text: "Nice to meet you" },
-  ],
-
-  DialogsData: [
-    { id: 1, name: "Dima" },
-    { id: 2, name: "Vova" },
-    { id: 3, name: "Sveta" },
-  ],
-
-}
-
-const DialogReducer = (state =initialStat, action) => {
-
-  switch (action.type) {
-    case ADD_MESSAGE: {
-      let newMessage = action.onChangeMessage
-        return {...state,
-
-          MessageData:[...state.MessageData, { id: 4, text: newMessage }]
-      }
-    }
-
-    default:
-      return state;
-  }
+	switch (action.type) {
+		case ADD_MESSAGE: {
+			let newMessage = action.onChangeMessage
+			return {
+				...state,
+				MessageData: [...state.MessageData, {id: 4, text: newMessage}],
+			}
+		}
+		default:
+			return state;
+	}
 };
 
 export const sendMessageActiveCreator = (onChangeMessage) => {
-  return { type: ADD_MESSAGE, onChangeMessage};
+	return {type: ADD_MESSAGE, onChangeMessage};
 };
 
 export default DialogReducer;
