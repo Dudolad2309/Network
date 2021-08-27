de5701a0e179bf773aa408d94938e2736dd5f407
Dudolad2309 <77323788+Dudolad2309@users.noreply.github.com>
2021-08-27 10:16:05 +0300

    Refactor

--- src/Redux/AppReducer.js
+++ src/Redux/AppReducer.js
@@ -1,43 +1,32 @@
 import {getAuth} from "./authReducer";
 
-
 const SET_INITIALIZED = "SET_INITIALIZED"
-
-
-let initialStat = {
-
-    initialized: false
-
-
-}
+const initialStat = {
+	initialized: false,
+};
 
 const AppReducer = (state = initialStat, action) => {
-
-    switch (action.type) {
-
-        case SET_INITIALIZED:
-            return {
-                ...state,
-                initialized: true
-            }
-        default:
-            return state;
-    }
+	switch (action.type) {
+		case SET_INITIALIZED:
+			return {
+				...state,
+				initialized: true,
+			}
+		default:
+			return state;
+	}
 };
 
 export const setInitializedSuccess = () => {
-
-    return {type: SET_INITIALIZED};
+	return {type: SET_INITIALIZED};
 };
 
 export const initializeApp = () => (dispatch) => {
-    let promise = dispatch(getAuth())
-    dispatch(setInitializedSuccess())
-    Promise.all([promise]).then(() => {
-        dispatch(setInitializedSuccess())
-    })
-}
-
-
+	let promise = dispatch(getAuth());
+	dispatch(setInitializedSuccess());
+	Promise.all([promise]).then(() => {
+		dispatch(setInitializedSuccess());
+	})
+};
 
 export default AppReducer;
