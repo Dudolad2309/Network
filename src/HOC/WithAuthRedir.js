e97c7d6f7c474c3ac5eaeb2cf7f17783322475c9
Dudolad2309 <77323788+Dudolad2309@users.noreply.github.com>
2021-08-27 10:14:19 +0300

    Refactor

--- src/HOC/WithAuthRedir.js
+++ src/HOC/WithAuthRedir.js
@@ -2,22 +2,19 @@ import React from "react";
 import {Redirect} from "react-router-dom";
 import {connect} from "react-redux";
 
-let mapStateToPropsForRedirect = (state) => ({
-
-    auth: state.auth.isAuth
-})
+const mapStateToPropsForRedirect = (state) => ({
+	auth: state.auth.isAuth
+});
 
 export const WithAuthRedirect = (Component) => {
-    class RedirectComponent extends React.Component {
-        render() {
-            if (!this.props.auth) return <Redirect to={"/Login"}/>;
-            return <Component {...this.props}/>
-        }
+	class RedirectComponent extends React.Component {
+		render() {
+			if (!this.props.auth) return <Redirect to={"/Login"}/>;
+			return <Component {...this.props}/>
+		}
+	}
 
-    }
+	const ConnectedAuthRedirectComponent = connect(mapStateToPropsForRedirect)(RedirectComponent);
 
-
-    let ConnectedAuthRedirectComponent = connect(mapStateToPropsForRedirect)(RedirectComponent)
-
-    return ConnectedAuthRedirectComponent
+	return ConnectedAuthRedirectComponent
 }
