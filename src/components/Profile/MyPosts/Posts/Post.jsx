f89c547487162db3aaf74f407cc1ab64b73a9808
Dudolad2309 <77323788+Dudolad2309@users.noreply.github.com>
2021-08-27 09:56:42 +0300

    Refactor

--- src/components/Profile/MyPosts/Posts/Post.jsx
+++ src/components/Profile/MyPosts/Posts/Post.jsx
@@ -2,17 +2,17 @@ import React from "react";
 import s from "./Post.module.css";
 
 const Post = (props) => {
-  return (
-    <div>
-      <div className={s.item}>
-        <img
-    src="https://s.pfst.net/2017.06/57345286756788a2d6aa37c7a739fe2b7beab61d3c94_b.jpg"
-    alt="1"
-    />
-        <div className={s.text}>{props.message}</div>
-        <div className={s.text}>Like {props.countLike}</div>
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			<div className={s.item}>
+				<img
+					src="https://s.pfst.net/2017.06/57345286756788a2d6aa37c7a739fe2b7beab61d3c94_b.jpg"
+					alt="1"
+				/>
+				<div className={s.text}>{props.message}</div>
+				<div className={s.text}>Like {props.countLike}</div>
+			</div>
+		</div>
+	);
 };
 export default Post;
