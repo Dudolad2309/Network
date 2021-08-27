ad5c6f76052717ed54bd9041276aa234522f3f66
Dudolad2309 <77323788+Dudolad2309@users.noreply.github.com>
2021-08-27 09:09:39 +0300

    Refactor

--- src/components/Dialogs/Message/Message.jsx
+++ src/components/Dialogs/Message/Message.jsx
@@ -2,6 +2,6 @@ import React from "react";
 import s from "./Message.module.css";
 
 const Message = (props) => {
-  return <div className={s.message}>{props.text}</div>;
+	return <div className={s.message}>{props.text}</div>;
 };
 export default Message;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,63 +1,44 @@
 import React from "react";
 import {NavLink} from "react-router-dom";
-import {
-    AppBar, Avatar,
-    Box,
-    Button,
-    Container,
-    IconButton,
-    makeStyles,
-    Toolbar,
-    Typography
-} from "@material-ui/core";
+import {AppBar, Avatar, Box, Button, Container, IconButton, makeStyles, Toolbar, Typography} from "@material-ui/core";
 import MenuIcon from "@material-ui/icons/Menu";
-
 
 const useStyles = makeStyles((theme) => ({
     menuButton: {
-        marginRight: theme.spacing(1)
+        marginRight: theme.spacing(1),
     },
     title: {
         flexGrow: 1,
-        position: "relative"
+        position: "relative",
     },
-    header:{
-        padding: theme.spacing(1)
+    header: {
+        padding: theme.spacing(1),
     }
-}))
+}));
 
 const Header = (props) => {
-    const classes = useStyles()
-
+    const classes = useStyles();
     return (
-        <AppBar className={classes.header} position="fixed">
-            <Container>
-                <Toolbar>
-                    <IconButton className={classes.menuButton}
-                                edge="start" color="inherit" aria-label="menu">
-                        <MenuIcon/>
-                    </IconButton>
-                    <Typography className={classes.title} variant="h5">Social Network</Typography>
-                    <Box marginRight={1}>
-                        {props.isAuth === true ?
-                            <Avatar alt="Remy Sharp" src="https://source.unsplash.com/random"/> : null}
-                    </Box>
-                    <Box>
-                        {props.isAuth === true
-                            ? <Typography> {props.login} <Button onClick={props.logout} color="secondary"
-                                                                 variant="contained">Log out</Button> </Typography>
-                            : <NavLink to={"/login"}><Button color="primary" variant="contained">Log
-                                in</Button></NavLink>}
-                    </Box>
-
-
-                </Toolbar>
-
-
-            </Container>
-
-
-        </AppBar>
+      <AppBar className={classes.header} position="fixed">
+          <Container>
+              <Toolbar>
+                  <IconButton className={classes.menuButton} edge="start" color="inherit" aria-label="menu">
+                      <MenuIcon/>
+                  </IconButton>
+                  <Typography className={classes.title} variant="h5">Social Network</Typography>
+                  <Box marginRight={1}>
+                      {props.isAuth === true ? <Avatar alt="Remy Sharp" src="https://source.unsplash.com/random"/> : null}
+                  </Box>
+                  <Box>
+                      {props.isAuth === true
+                        ? <Typography> {props.login} <Button onClick={props.logout} color="secondary"
+                                                             variant="contained">Log out</Button> </Typography>
+                        : <NavLink to={"/login"}><Button color="primary" variant="contained">Log
+                            in</Button></NavLink>}
+                  </Box>
+              </Toolbar>
+          </Container>
+      </AppBar>
     );
 };
 export default Header;
