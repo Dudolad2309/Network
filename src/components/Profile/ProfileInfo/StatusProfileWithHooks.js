4d287f32c98142cb1dbc551a069fef4fc530aa5b
Dudolad2309 <77323788+Dudolad2309@users.noreply.github.com>
2021-08-27 09:41:23 +0300

    Refactor

--- src/components/Profile/ProfileInfo/StatusProfileWithHooks.js
+++ src/components/Profile/ProfileInfo/StatusProfileWithHooks.js
@@ -1,51 +1,41 @@
 import React, {useEffect, useState} from "react";
 import {Container, Input, Typography} from "@material-ui/core";
 
-
 const StatusProfileWithHooks = (props) => {
+	const [editMode, setEditMode] = useState(false);
+	const [status, setStatus] = useState(props.status);
+	useEffect(() => {
+		setStatus(props.status);
+	}, [props.status]);
 
-    let [editMode, setEditMode] = useState(false)
-    let [status, setStatus] = useState(props.status)
+	const activeEditMode = () => {
+		setEditMode(true);
+	};
 
-    useEffect(() => {
-        setStatus(props.status)
-    }, [props.status])
+	const deactivationEditMode = () => {
+		setEditMode(false);
+		props.updateStatus(status);
+	};
 
-    let activeEditMode = () => {
-        setEditMode(true)
-    }
+	const onStatusChange = (e) => {
+		setStatus(e.currentTarget.value);
+	};
 
-    const deactivationEditMode = () => {
-        setEditMode(false)
-        props.updateStatus(status)
-    }
-
-    const onStatusChange = (e) => {
-        setStatus(e.currentTarget.value)
-
-    }
-
-
-    return (
-
-
-        <Container>
-            {!editMode &&
-            <Typography variant="h4" onDoubleClick={activeEditMode}>
-                {props.status || "No status"}
-            </Typography>
-            }
-            {editMode &&
-            <Typography variant="h4" paragraph>
-                <Input color="secondary" defaultValue={status} onChange={onStatusChange} onBlur={deactivationEditMode}
-                       autoFocus={true}/>
-            </Typography>
-            }
-        </Container>
-
-
-    );
-
+	return (
+		<Container>
+			{!editMode &&
+			<Typography variant="h4" onDoubleClick={activeEditMode}>
+				{props.status || "No status"}
+			</Typography>
+			}
+			{editMode &&
+			<Typography variant="h4" paragraph>
+				<Input color="secondary" defaultValue={status} onChange={onStatusChange} onBlur={deactivationEditMode}
+				       autoFocus={true}/>
+			</Typography>
+			}
+		</Container>
+	);
 }
 export default StatusProfileWithHooks;
 
