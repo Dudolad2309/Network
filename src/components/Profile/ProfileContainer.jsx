d691e53b52f8e5c379364750035f13d2b9297ed1
Dudolad2309 <77323788+Dudolad2309@users.noreply.github.com>
2021-08-27 09:34:32 +0300

    Refactor

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -6,53 +6,37 @@ import {connect} from "react-redux";
 import {withRouter} from "react-router-dom";
 import {compose} from "redux";
 
-
 class ProfileContainer extends React.Component {
+	componentDidMount() {
+		let userId = this.props.match.params.userId
+		if (!userId) {
+			userId = this.props.authId ? this.props.authId : this.props.history.push("/Login")
+		}
+		this.props.getProfile(userId)
+		this.props.getStatus(userId)
+	}
 
-    componentDidMount() {
-
-        let userId = this.props.match.params.userId
-        if (!userId) {
-
-            userId = this.props.authId ? this.props.authId : this.props.history.push("/Login")
-
-        }
-
-        this.props.getProfile(userId)
-
-        this.props.getStatus(userId)
-
-    }
-
-    render() {
-
-        return (
-
-
-            <div className={s.use}>
-                <Profile {...this.props} profile={this.props.profile}
-                         status={this.props.status}  updateStatus={this.props.updateStatus}/>
-
-            </div>
-        );
-    }
+	render() {
+		return (
+			<div className={s.use}>
+				<Profile {...this.props} profile={this.props.profile}
+				         status={this.props.status} updateStatus={this.props.updateStatus}/>
+			</div>
+		);
+	}
 }
 
+const mapStateToProps = (state) => ({
+	profile: state.Profile.profile,
+	status: state.Profile.status,
+	authId: state.auth.id,
+	isAuth: state.auth.isAuth,
+});
 
-
-
-let mapStateToProps = (state) => ({
-    profile: state.Profile.profile,
-    status: state.Profile.status,
-    authId:state.auth.id,
-    isAuth:state.auth.isAuth
-})
-
-
-
-
-export default compose(  withRouter,
-    connect(mapStateToProps, {setUserProfile, getProfile,getStatus,updateStatus}))(ProfileContainer)
+export default compose
+(withRouter,
+	connect(mapStateToProps,
+		{setUserProfile, getProfile, getStatus, updateStatus}))(ProfileContainer)
 
 
 
